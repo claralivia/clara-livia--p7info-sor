97f1097bc61e384006fc9d0db3658c73d5bf298f
unknown <clara.livia.moura62@aluno.ifce.edu.br>
2021-10-17 19:48:56 -0300

    SOR

--- atividades/echo-server/IMC/client.py
+++ atividades/echo-server/IMC/client.py
@@ -32,7 +32,7 @@ class App(Tk):
         s.close()
         texto = "O resultado do seu IMC é {:.2f},\nvocê está {}".format(answer['imc'], answer['msg'])
         self.lblAnswer = Label(self.container5, text=texto,
-                               font="Helvetica 12 italic",
+                               font="Helvetica 12",
                                bg='white')
         self.lblAnswer.pack()
 
@@ -46,6 +46,10 @@ class App(Tk):
     def initialize(self):
         self.fonte = "Helvetica 12 bold"
 
+        self.container0 = Frame(None, padx=0, pady=0, bg='white')
+        self.container0.pack()
+        self.container00 = Frame(None, padx=20, pady=20, bg='white')
+        self.container00.pack()
         self.container1 = Frame(None, padx=20, pady=10, bg='white')
         self.container1.pack()
         self.container2 = Frame(None, padx=20, pady=5, bg='white')
@@ -54,8 +58,26 @@ class App(Tk):
         self.container3.pack()
         self.container4 = Frame(None, padx=20, pady=10, bg='white')
         self.container4.pack()
-        self.container5 = Frame(None, padx=20, pady=15, bg='white')
+        self.container5 = Frame(None, padx=20, pady=30, bg='white')
         self.container5.pack()
+        self.container6 = Frame(None, padx=0, pady=0, bg='white')
+        self.container6.pack()
+
+        self.color1 = Label(self.container0, bg='#FF4500', width=20)
+        self.color1.pack(side=LEFT)
+        self.color2 = Label(self.container0, bg='#FFD700', width=20)
+        self.color2.pack(side=LEFT)
+        self.color3 = Label(self.container0, bg='#2E8B57', width=20)
+        self.color3.pack(side=LEFT)
+
+        self.color4 = Label(self.container6, bg='#FF4500', width=20)
+        self.color4.pack(side=LEFT)
+        self.color5 = Label(self.container6, bg='#FFD700', width=20,
+                            text="Por Clara Lívia", font="Helvetica 9",
+                            fg="white")
+        self.color5.pack(side=LEFT)
+        self.color6 = Label(self.container6, bg='#2E8B57', width=20)
+        self.color6.pack(side=LEFT)
 
         self.titlee = Label(self.container1, text="Informe os dados:",
                            font="Helvetica 16 bold",
@@ -104,6 +126,10 @@ class App(Tk):
                                bg="#2E8B57",
                                fg="white")
         self.bntClear.pack(side=LEFT)
+
+        self.lblAnswer = Label(self.container00, text="",
+                               bg='white')
+        self.lblAnswer.pack()
 
         self.lblAnswer = Label(self.container5, text="",
                                bg='white')
